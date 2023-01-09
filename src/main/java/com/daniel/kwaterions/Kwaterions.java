1229631be50566bf42e97dcde3cf96ed004f7f28
Daniel Dobrzyński <35843305+DanielDob@users.noreply.github.com>
2023-01-09 18:02:52 +0100

    added lines and cleaning

--- src/main/java/com/daniel/kwaterions/Kwaterions.java
+++ src/main/java/com/daniel/kwaterions/Kwaterions.java
@@ -6,8 +6,10 @@ import javafx.application.Application;
 import javafx.application.Platform ;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
@@ -21,11 +23,14 @@ public class Kwaterions extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        scene = new Scene(loadFXML("primary"), ReadOnly.X, ReadOnly.Y);
+        scene = new Scene(loadFXML("/primary"), ReadOnly.X, ReadOnly.Y);
         scene.setOnKeyPressed(escape());
         stage.setFullScreen(true);
         stage.setScene(scene);
         stage.show();
+        ((Canvas)((Group)scene.getRoot()).getChildren().get(0)).setWidth(ReadOnly.X);
+        ((Canvas)((Group)scene.getRoot()).getChildren().get(0)).setHeight(ReadOnly.Y);
+        new Graph(((Canvas)((Group)scene.getRoot()).getChildren().get(0)));
     }
 
     
